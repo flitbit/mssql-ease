2168f42a7652bd3a7a53bce893cf18ceab9713cd
Phillip Clark <phillip@flitbit.com>
2018-10-15 15:12:42 -0600

    updated packages

--- examples/readme-inspect-dbobjects.js
+++ examples/readme-inspect-dbobjects.js
@@ -14,22 +14,22 @@ mssql.create({
   maxPooledConnections: 100,
   idleTimeoutMillis: 5000
 }, true)
-.then(pool => {
-  pool.connect(config)
-    .then(cn => {
-      let sqlFile = path.normalize(path.join(__dirname, '../lib/tsql/dbobjects.sql'));
-      let data = [];
-      return xutil.loadFile(sqlFile, 'utf8')
-        .then(query =>
-          cn.queryObjects(query, (obj) => {
-            data.push(obj);
-          })
-          .then(stats => {
-            cn.release();
-            data.forEach(d => util.log(util.inspect(d, false, 9)));
-            util.log(`row count: ${stats.rowCount}, time: ${stats.hrtime[0]}s ${stats.hrtime[1] / 1000000}ms`);
-          }));
-    })
-    .catch(err => util.log(util.inspect(err, false, 9)))
-    .then(() => pool.drain());
-});
+  .then(pool => {
+    pool.connect(config)
+      .then(cn => {
+        let sqlFile = path.normalize(path.join(__dirname, '../lib/tsql/dbobjects.sql'));
+        let data = [];
+        return xutil.loadFile(sqlFile, 'utf8')
+          .then(query =>
+            cn.queryObjects(query, (obj) => {
+              data.push(obj);
+            })
+              .then(stats => {
+                cn.release();
+                data.forEach(d => util.log(util.inspect(d, false, 9)));
+                util.log(`row count: ${stats.rowCount}, time: ${stats.hrtime[0]}s ${stats.hrtime[1] / 1000000}ms`);
+              }));
+      })
+      .catch(err => util.log(util.inspect(err, false, 9)))
+      .then(() => pool.drain());
+  });
